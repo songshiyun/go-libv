5743df2fa6a219cd554e7ba1f892202bb9b5f07c
songshiyun99@gmial.com <songshiyun99@gmail.com>
2020-03-29 01:31:17 +0800

    crc16

--- crypt/hash_test.go
+++ crypt/hash_test.go
@@ -7,7 +7,13 @@ import (
 )
 
 func TestTime33(t *testing.T) {
-	for i:=0;i<100000 ;i++  {
-		fmt.Println(Time33("test_hash_"+strconv.Itoa(i)))
+	for i := 0; i < 100000; i++ {
+		fmt.Println(Time33("test_hash_" + strconv.Itoa(i)))
+	}
+}
+
+func TestCRC16(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(CRC16("test_hash_" + strconv.Itoa(i)))
 	}
 }
